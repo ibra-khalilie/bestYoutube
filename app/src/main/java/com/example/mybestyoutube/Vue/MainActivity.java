6cec5fb4abe6eeb154089b8a4081e521b0ffcba0
Yaya Sakho <yahyasakho97a@gmail.com>
2023-03-05 12:00:49 +0100

    Initialisation du projet

---
+++ app/src/main/java/com/example/mybestyoutube/Vue/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mybestyoutube.Vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mybestyoutube.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
